8891487d8b24e3d7b355aa09db2168025af36478
Blake Watkins <iamsoblake@gmail.com>
2016-08-17 21:24:24 -0700

    still figuring out movement

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+  //variable store//
   let vendorUrl = window.URL || window.webkitURL,
     peer,
     chattersClient = [],
     chatterThisClient,
     video,
     canvas,
-    context;
+    context,
+    button = document.getElementById('filter'),
+    current = document.getElementById('filterDisp'),
+    filters = ['blur(5px)', 'brightness(0.4)', 'contrast(200%)', 'grayscale(100%)', 'hue-rotate(90deg)', 'invert(100%)', 'sepia(100%)', 'saturate(20)', 'none'],
+    i = 0,
+    raf;
 
+  let emoImg = new Image();
+  emoImg.src = 'assets/smLoveTongue.png';
+
+  //variable store//
+
+  //vendor media objects//
   navigator.getMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||
     navigator.msGetUserMedia;
+  //vendor media objects//
+
 
   navigator.getMedia({
     video: true,
@@ -27,9 +41,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
     myVideo.src = window.URL.createObjectURL(stream);
     myVideo.play();
 
-    myVideo.addEventListener('play', function() {
-      draw(this, context, 400, 300);
-    }, false);
+    // myVideo.addEventListener('play', function() {
+    // draw(this, context, 400, 300);
+    // }, false);
 
     socket.emit('initiator?', JSON.stringify(stream.id));
     socket.on('initiated', (chatter) => {
@@ -126,19 +140,82 @@ document.addEventListener("DOMContentLoaded", function(event) {
         document.getElementById('innerbooth').appendChild(canvas);
 
         context = canvas.getContext('2d');
+        canvas.width = 640;
+        canvas.height = 480;
+        context.rect(0, 0, canvas.width, canvas.height);
+        context.stroke();
+        canvas.style.visibility = "visible";
 
+
+
+
+        //leave just in case
         video.addEventListener('play', function() {
-          canvas.width = 640;
-          canvas.height = 480;
 
-          make_base(this, context, canvas.width, canvas.height)
-          // context.drawImage(this, 0, 0, width, height);
+          // make_base(this, context, canvas.width, canvas.height)
           // draw(this, context, canvas.width, canvas.height);
         }, false);
 
-        video.addEventListener('progress', function() {
-          var show = video.currentTime >= 5 && video.currentTime < 10;
-          canvas.style.visibility = "visible";
+        //enables overlay, may be unnecessary//
+        video.addEventListener('progress', function() {}, false);
+        //enables overlay//
+
+        //tesing MDN example//
+
+
+        //test to manipulate canvas insertion and coordinate extraction
+        canvas.addEventListener('click', function(event) {
+            // console.log(getCursorPosition(canvas, event), 'event: ', event);
+            // tesing for getting objects to move
+            let emoticon = {
+              x: getCursorPosition(canvas, event).x,
+              y: getCursorPosition(canvas, event).y,
+              vx: 5,
+              vy: 2,
+              draw: function() {
+                var self = this;
+                emoImg.onload = function() {
+                  // console.log('img hi')
+                  context.drawImage(baseImg, self.x - emoImg.width / 2, self.y - emoImg.height / 2);
+                }
+              }
+            };
+
+            raf = window.requestAnimationFrame(draw);
+
+            if (emoticon.y + emoticon.vy > canvas.height || emoticon.y + emoticon.vy < 0) {
+              emoticon.vy = -emoticon.vy;
+            }
+            if (emoticon.x + emoticon.vx > canvas.width || emoticon.x + emoticon.vx < 0) {
+              emoticon.vx = -emoticon.vx;
+            }
+            // emoticon.draw()
+
+            function draw() {
+              console.log(emoticon)
+              context.clearRect(0, 0, canvas.width, canvas.height);
+              emoticon.draw();
+              emoticon.x += emoticon.vx;
+              emoticon.y += emoticon.vy;
+              raf = window.requestAnimationFrame(draw);
+            }
+
+            //tesing for putting img on canvas
+            // var position = getCursorPosition(canvas, event);
+            // paste(this, context, canvas.width, canvas.height, position.x, position.y)
+          }, false)
+          //////////////
+
+        //tesing filters//
+        button.addEventListener('click', function() {
+          console.log('filter please: ' + filters[i]);
+          current.innerHTML = filters[i];
+          video.style.webkitFilter = filters[i];
+          video.style.mozFilter = filters[i];
+          video.style.filter = filters[i];
+
+          i++;
+          if (i >= filters.length) i = 0;
         }, false);
 
       });
@@ -151,38 +228,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 
-  function draw(video, context, width, height) {
-    var image, data, i, r, g, b, brightness;
-
+  function paste(video, context, width, height, x, y) {
     context.drawImage(video, 0, 0, width, height);
-
-    image = context.getImageData(0, 0, width, height);
-    data = image.data;
-
-    for (i = 0; i < data.length; i = i + 4) {
-      r = data[i];
-      g = data[i + 1];
-      b = data[i + 2];
-      brightness = (r + b + g);
-
-      data[i] = data[i + 1] = data[i + 2] = brightness;
+    baseImg = new Image();
+    baseImg.src = 'assets/weird.png';
+    baseImg.onload = function() {
+      console.log(baseImg.width, baseImg.height);
+      context.drawImage(baseImg, x - baseImg.width / 2, y - baseImg.height / 2);
+      // var time = window.setTimeout(function() {
+      // context.clearRect(x - baseImg.width / 2, y - baseImg.height / 2, baseImg.width, baseImg.height);
+      // }, 5000);
     }
-
-    image.data = data;
-
-    context.putImageData(image, 0, 0);
-
-    // make_base();
-    setTimeout(draw, 10, video, context, width, height);
   }
 
-  function make_base(video, context, width, height) {
-    context.drawImage(video, 0, 0, width, height);
-    base_image = new Image();
-    base_image.src = 'assets/twistedFace.png';
-    base_image.onload = function() {
-      context.drawImage(base_image, 50, 50);
-    }
+  function getCursorPosition(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    var x = event.clientX - rect.left;
+    var y = event.clientY - rect.top;
+    var pos = {
+      x: x,
+      y: y
+    };
+    return pos;
+    // console.log("x: " + x + " y: " + y, pos);
   }
 
 });
