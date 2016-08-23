b5aa0288ebc88745cfb098555f21852c119dfeea
Blake Watkins <iamsoblake@gmail.com>
2016-08-22 17:49:36 -0700

    another small mistake fixed

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ const options = {
   cert: fs.readFileSync('server.crt')
 };
 
-const io = require('socket.io').listen(server);
 
 const server = https.createServer(options, app).listen(config.port, function() {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
 
+const io = require('socket.io').listen(server);
 
 const connections = [];
 const rooms = [];
