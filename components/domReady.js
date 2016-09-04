5ce6e985863deffec8c85132de1d086c45e63547
kevinl <kevinliu987@gmail.com>
2016-09-03 19:51:17 -0700

    resolving merge

---
+++ components/domReady.js
@@ -0,0 +1,54 @@
+let domReady = function(callback) {
+    let ready = false;
+
+    let detach = function() {
+        if(document.addEventListener) {
+            document.removeEventListener("DOMContentLoaded", completed);
+            window.removeEventListener("load", completed);
+        } else {
+            document.detachEvent("onreadystatechange", completed);
+            window.detachEvent("onload", completed);
+        }
+    }
+    let completed = function() {
+        if(!ready && (document.addEventListener || event.type === "load" || document.readyState === "complete")) {
+            ready = true;
+            detach();
+            callback();
+        }
+    };
+
+    if(document.readyState === "complete") {
+        callback();
+    } else if(document.addEventListener) {
+        document.addEventListener("DOMContentLoaded", completed);
+        window.addEventListener("load", completed);
+    } else {
+        document.attachEvent("onreadystatechange", completed);
+        window.attachEvent("onload", completed);
+
+        let top = false;
+
+        try {
+            top = window.frameElement == null && document.documentElement;
+        } catch(e) {}
+
+        if(top && top.doScroll) {
+            (function scrollCheck() {
+                if(ready) return;
+
+                try {
+                    top.doScroll("left");
+                } catch(e) {
+                    return setTimeout(scrollCheck, 50);
+                }
+
+                ready = true;
+                detach();
+                callback();
+            })();
+        }
+    }
+};
+
+export { domReady };
